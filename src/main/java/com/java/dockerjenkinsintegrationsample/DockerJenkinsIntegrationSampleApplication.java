f570c64c4835b37c5cc4734c68bf77f37c184c10
balaji sijapati <ram40193>
2020-08-06 11:53:37 +0530

    this is to test entire flow

--- src/main/java/com/java/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/java/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -8,7 +8,7 @@ public class DockerJenkinsIntegrationSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
-		System.out.println("Testing complete flow started docker in local and env change");
+		System.out.println("Testing complete flow started docker in local and env change with date august 6 2020");
 	}
 
 }
