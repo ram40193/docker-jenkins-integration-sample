c745273a5c021e117ee6c23d9005f5eea7b9dfa6
balaji sijapati <ram40193>
2020-08-06 12:53:46 +0530

    this is to test entire flow 3

--- src/main/java/com/java/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/java/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -8,7 +8,7 @@ public class DockerJenkinsIntegrationSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
-		System.out.println(" env change with date august 6 2020");
+		System.out.println("august 6 2020");
 	}
 
 }
